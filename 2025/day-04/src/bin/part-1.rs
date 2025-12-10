19aa511137fbe592573b3996c9c03873bc95a3de
Stanislav Stoyanov <grimerssy@gmail.com>
2025-12-10 23:04:16 +0100

    prefix days 1-9 with 0 ensures consistent order in file explorers

---
+++ 2025/day-04/src/bin/part-1.rs
@@ -0,0 +1,13 @@
+use day_04::part_1::solve;
+use miette::Context;
+use tracing_subscriber::EnvFilter;
+
+fn main() -> miette::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+    let file = include_str!("../../../../input.txt");
+    let solution = solve(file).context("solve part 1")?;
+    println!("{solution}");
+    Ok(())
+}
---
+++ 2025/day-07/src/bin/part-2.rs
@@ -0,0 +1,13 @@
+use day_07::part_2::solve;
+use miette::Context;
+use tracing_subscriber::EnvFilter;
+
+fn main() -> miette::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+    let file = include_str!("../../../../input.txt");
+    let solution = solve(file).context("solve part 2")?;
+    println!("{solution}");
+    Ok(())
+}
